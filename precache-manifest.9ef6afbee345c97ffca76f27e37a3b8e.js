9c7f35bd009d3cca79ed69f7339b5b5d1b797f60
Max Soloviov <solovyov.amron@gmail.com>
2019-05-18 19:31:53 +0300

    aaa

---
+++ precache-manifest.9ef6afbee345c97ffca76f27e37a3b8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "13b5a2ad4b3a012105b5",
+    "url": "/static/js/main.13b5a2ad.chunk.js"
+  },
+  {
+    "revision": "3c16ad591fd0d9841a14",
+    "url": "/static/js/1.3c16ad59.chunk.js"
+  },
+  {
+    "revision": "13b5a2ad4b3a012105b5",
+    "url": "/static/css/main.5547b9bf.chunk.css"
+  },
+  {
+    "revision": "f39568185b038ad2b876b68d2d3933f0",
+    "url": "/index.html"
+  }
+];
